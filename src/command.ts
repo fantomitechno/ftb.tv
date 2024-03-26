ef9ddd20a7d72ef559aba813152b96da3f5801ea
Simon - fantomitechno <simon@renoux.dev>
2024-03-26 12:49:39 +0100

    ♻️ Redid some functions

--- src/command.ts
+++ src/command.ts
@@ -10,7 +10,7 @@ type Command = {
   value: string;
 };
 
-export const getCommand = async (command: string) => {
+const getFile = async () => {
   const path = `./commands.toml`;
   if (!fileExists(path)) {
     console.log(`Creating commands.toml`);
@@ -23,28 +23,20 @@ export const getCommand = async (command: string) => {
     writeFileSync(path, stringify(data));
     return null;
   }
+  return readFileSync(path, "utf-8");
+}
 
-  const input = readFileSync(path, "utf-8");
+export const getCommand = async (command: string) => {
+  const input = await getFile();
+  if (!input) return null;
   const data = parse(input);
   const cmd = data[command];
   return cmd as Command;
 };
 
 export const commandList = async () => {
-  const path = `./commands.toml`;
-  if (!fileExists(path)) {
-    console.log(`Creating commands.toml`);
-    const data: { [key: string]: Command } = {
-      "example": {
-        value: "This is an example command",
-        respond: false
-      }
-    };
-    writeFileSync(path, stringify(data));
-    return null;
-  }
-
-  const input = readFileSync(path, "utf-8");
+  const input = await getFile();
+  if (!input) return [];
   const data = parse(input);
   return Object.keys(data);
 }
